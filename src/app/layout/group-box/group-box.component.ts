d77f0204cd727fd0fc05dad2a888756dc3b4cd0f
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2025-09-17 10:29:13 +0200

    update to ng 20

--- src/app/layout/group-box/group-box.component.ts
+++ src/app/layout/group-box/group-box.component.ts
@@ -1,11 +1,9 @@
 import {Component, Input} from '@angular/core';
-import {NgIf} from '@angular/common';
+
 
 @Component({
   selector: 'sr-group-box',
-  imports: [
-    NgIf
-  ],
+  imports: [],
   templateUrl: './group-box.component.html',
   styleUrl: './group-box.component.scss',
   standalone: true
