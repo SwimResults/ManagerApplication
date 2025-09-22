4c0fb35609226ad9a4c3f2cda2a4a5ce87133ad4
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2025-09-22 12:41:56 +0200

    add router link active to icon buttons

--- src/app/layout/element/icon-button/icon-button.component.ts
+++ src/app/layout/element/icon-button/icon-button.component.ts
@@ -1,16 +1,20 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {MatIcon} from '@angular/material/icon';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-icon-button',
   imports: [
-    MatIcon
+    MatIcon,
+    RouterLinkActive,
+    RouterLink
   ],
   templateUrl: './icon-button.component.html',
   styleUrl: './icon-button.component.scss'
 })
 export class IconButtonComponent {
   @Input() icon: string = "dashboard";
+  @Input() btnRouterLink?: string;
 
   @Output() btnClicked = new EventEmitter<PointerEvent>();
 
