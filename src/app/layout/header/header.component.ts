8913e911255094fdee0f1c07bae3e8f1d9c0f84f
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2025-12-17 07:55:03 +0100

    add auth config and oidc dependency

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {IconButtonComponent} from '../element/icon-button/icon-button.component';
 
 @Component({
-  selector: 'app-header',
-  imports: [
-    IconButtonComponent
-  ],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+    selector: 'app-header',
+    imports: [
+        IconButtonComponent
+    ],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
