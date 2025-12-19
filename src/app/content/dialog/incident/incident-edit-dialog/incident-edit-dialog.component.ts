e4ada656dcf76d197111284a42fe49d7a8be39e6
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2025-12-19 09:45:14 +0100

    add logic to save incidents

--- src/app/content/dialog/incident/incident-edit-dialog/incident-edit-dialog.component.ts
+++ src/app/content/dialog/incident/incident-edit-dialog/incident-edit-dialog.component.ts
@@ -15,6 +15,8 @@ import {TranslatePipe} from '@ngx-translate/core';
 import {MeetingImpl} from '../../../../core/model/meeting/meeting.model';
 import {EventService} from '../../../../core/service/api';
 import {MeetingEvent} from '../../../../core/model/meeting/meeting-event.model';
+import {IncidentService} from '../../../../core/service/api/meeting/incident.service';
+import {SnackBarService} from '../../../../core/service/ui/snack-bar.service';
 
 export interface IncidentEditDialogData {
     incident: IncidentImpl;
@@ -43,6 +45,8 @@ export class IncidentEditDialogComponent implements OnInit {
     data = inject<IncidentEditDialogData>(MAT_DIALOG_DATA);
 
     private eventService = inject(EventService)
+    private incidentService = inject(IncidentService)
+    private snackBarService = inject(SnackBarService)
 
     events?: MeetingEvent[];
 
@@ -54,13 +58,18 @@ export class IncidentEditDialogComponent implements OnInit {
         "CEREMONY",
     ];
 
+    incident: IncidentImpl;
+
     constructor() {
         if (!this.data.incident) {
-            this.data.incident = new IncidentImpl({
+            this.incident = new IncidentImpl({
                 type: "EVENT",
                 start: "2025-12-20T07:15:00Z",
                 end: "2025-12-20T08:15:00Z",
+                meeting: this.data.meeting.meet_id
             } as Incident);
+        } else {
+            this.incident = this.data.incident;
         }
     }
 
@@ -70,4 +79,24 @@ export class IncidentEditDialogComponent implements OnInit {
         })
     }
 
+    saveIncident() {
+        if (this.data.incident) {
+            this.incidentService.updateIncident(this.incident).subscribe(this.handleSaveResult)
+        } else {
+            this.incidentService.addIncident(this.incident).subscribe(this.handleSaveResult)
+        }
+    }
+
+    private handleSaveResult() {
+        return {
+            next: (incident: Incident) => {
+                this.dialogRef.close(incident);
+                this.snackBarService.open("Gespeichert!")
+            },
+            error: (err: any) => {
+                this.snackBarService.open("Fehler beim Speichern!")
+                console.log(err)
+            }
+        };
+    }
 }
