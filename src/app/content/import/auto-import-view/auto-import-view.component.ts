3d9b21a03596e7fa8ed2d30d55ed6c9eda14917b
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2026-04-17 10:28:12 +0200

    add multiple auto importers (ui only)

--- src/app/content/import/auto-import-view/auto-import-view.component.ts
+++ src/app/content/import/auto-import-view/auto-import-view.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import {AutoImportToolComponent} from "../auto-import-tool/auto-import-tool.component";
+import {GroupBoxComponent} from '../../../layout/group-box/group-box.component';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-auto-import-view',
     imports: [
-        AutoImportToolComponent
+        AutoImportToolComponent,
+        GroupBoxComponent,
+        MatIcon
     ],
   templateUrl: './auto-import-view.component.html',
   styleUrl: './auto-import-view.component.scss'
 })
 export class AutoImportViewComponent {
+    autoImporters: number[] = [1];
 
+    addAutoImporter() {
+        this.autoImporters.push(this.autoImporters.length + 1);
+    }
+
+    removeAutoImporter(importer: number) {
+        this.autoImporters = this.autoImporters.filter(i => i !== importer);
+    }
 }
